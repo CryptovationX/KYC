01e3560f6332b457a97df6dbbc712666ff540724
WorakornX <worakorn.wattanakulchart@gmail.com>
2018-06-14 19:29:14 +0700

    create home in view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+
+    <title>CryptovationX | KYC</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row" style="padding-top:20px">
+            <p>test</p>
+        </div>
+        <hr>
+        <div class="row">
+            <h1>test header</h1>
+        </div>
+    </div>
+</body>
+</html>
